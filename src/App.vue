3b065b9e9d613db22cc4d2a35af569b44c3c1276
Sabino Hernandez Paulino <sakuraeyes21@gmail.com>
2023-04-27 17:52:28 -0600

    Inicializacion de vue router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-link :to="{name:'home'}">Home</router-link>
+  <router-link :to="{name:'about'}">About</router-link>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
