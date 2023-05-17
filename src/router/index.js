080401b4b49e343bc64a5c00e2979aaed9843bbe
Sabino Hernandez Paulino <sakuraeyes21@gmail.com>
2023-05-17 12:12:39 -0600

    rutas con metadatos

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,13 @@ const router=createRouter({
 history:createWebHistory(),
 routes:[
     {
-        path:'/',name:'home',component:HomeView, alias:['/home','/inicio']
+        path:'/',
+        name:'home',
+        component:HomeView,
+        alias:['/home','/inicio'],
+        meta:{
+            requiresAuth:false
+        }
     },
     {
         //path:'/home',redirect:'/',
@@ -26,7 +32,13 @@ routes:[
         path:'/about',name:'about',component:()=>import('@/views/AboutView.vue'),
     },
     {
-        path:'/chat',name:'chat',component:()=>import('@/views/ChatView.vue'),
+        path:'/chat',
+        name:'chat',
+        component:()=>import('@/views/ChatView.vue'),
+        meta:{
+            requiresAuth:true,
+            roles:['admin'],
+        },
         children:[
             {
                 path:':chatId',
@@ -53,6 +65,10 @@ routes:[
 router.beforeEach((to,from)=>{
     console.log("to",to);
     console.log("from",from)
+    if(to.meta?.requiresAuth && to.meta?.roles.includes('admin')){
+        console.log(to.path, ' requires auth')
+        return {name:'session'}
+    }
 
     if(to.path==='/'){
         //return '/about'
